ee708b0159e82ab701ddbd67ef1fa8d73ccbf5c4
RomainMarceDesj <desjonqueres@keio.jp>
2026-04-01 22:39:53 +0900

    attempted fix for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,   // avoid clash with V3 on 3000
+    port: 3001,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
   },
 })
